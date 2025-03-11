b3f05fcd51ea7d3a2fca5889c9d737d311f92e7b
fatmaelzahraaali <fatma.elzahraa.ali.75@gmail.com>
2025-03-11 09:38:22 +0200

    lab2 modifications

--- ITIan/course_app/models.py
+++ ITIan/course_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=25)
+    course_id= models.IntegerField(primary_key=True, default=0)
 
     def __str__(self):
         return self.name
--- ITIan/trainee_app/models.py
+++ ITIan/trainee_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Trainee(models.Model):
     name = models.CharField(max_length=255)
+    id = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.name
